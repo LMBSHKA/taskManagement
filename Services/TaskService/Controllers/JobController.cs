46e7a5becea60cdf88d77254ec84e8a72f4b68d1
LMBSHKA <makstanc2013@yandex.ru>
2025-04-27 11:52:06 +0500

    fix: made async controllers

--- Services/TaskService/Controllers/JobController.cs
+++ Services/TaskService/Controllers/JobController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using TaskService.Database;
 using TaskService.DTOs;
 using TaskService.Models;
@@ -18,12 +19,12 @@ namespace TaskService.Controllers
 		}
 
 		[HttpGet("tasks")]
-		public IActionResult GetAllJobs([FromQuery] JobFilterDTO jobFilter, [FromQuery] int pageNumber = 1)
+		public async Task<IActionResult> GetAllJobs([FromQuery] JobFilterDTO jobFilter, [FromQuery] int pageNumber = 1)
 		{
 			if (pageNumber <= 0)
 				return BadRequest("Invalid page number");
 
-			var jobs = _jobRepo.GetAllJobs(jobFilter, pageNumber).Result;
+			var jobs = await _jobRepo.GetAllJobs(jobFilter, pageNumber);
 			if (jobs == null || jobs.Count == 0)
 				return NotFound("Jobs not found");
 
@@ -31,9 +32,9 @@ namespace TaskService.Controllers
 		}
 
 		[HttpGet("tasks/{id}")]
-		public IActionResult GetJobById([FromQuery] int id)
+		public async Task<IActionResult> GetJobById(int id)
 		{
-			var job = _jobRepo.GetJobById(id).Result;
+			var job = await _jobRepo.GetJobById(id);
 			if (job == null)
 				return NotFound("job does not exist");
 
@@ -41,45 +42,45 @@ namespace TaskService.Controllers
 		}
 
 		[HttpPost("tasks")]
-		public IActionResult CreateJob([FromBody] Job newJob)
+		public async Task<IActionResult> CreateJob([FromBody] Job newJob)
 		{
 			if (newJob == null)
 				return BadRequest("Job data is invalid");
 			
-			if (_jobRepo.CreateJob(newJob).Result)
+			if (await _jobRepo.CreateJob(newJob))
 				return Ok("Job created");
 
 			return BadRequest("Job does not created");
 		}
 
 		[HttpPut("tasks/{id}")]
-		public IActionResult UpdateJob([FromQuery] int id, [FromBody] UpdateJobDTO updateData)
+		public async Task<IActionResult> UpdateJob(int id, [FromBody] UpdateJobDTO updateData)
 		{
 			if (updateData == null)
 				return BadRequest("Update data invalid");
 
-			if (_jobRepo.UpdateJob(id, updateData).Result)
+			if (await _jobRepo.UpdateJob(id, updateData))
 				return Ok();
 
 			return BadRequest("Update failed");
 		}
 
 		[HttpDelete("tasks/{id}")]
-		public IActionResult DeleteJob([FromQuery] int id)
+		public async Task<IActionResult> DeleteJob(int id)
 		{
-			if (_jobRepo.DeleteJob(id).Result)
+			if (await _jobRepo.DeleteJob(id))
 				return Ok();
 
 			return BadRequest("deletion failed");
 		}
 
 		[HttpPut("tasks/{id}/assign")]
-		public IActionResult SetExecutor([FromQuery] int id, [FromBody] string executor)
+		public async Task<IActionResult> SetExecutor(int id, [FromBody] string executor)
 		{
 			if (String.IsNullOrEmpty(executor))
 				return BadRequest("Executor's data invalid");
 
-			if (_jobRepo.SetExecutor(id, executor).Result)
+			if (await _jobRepo.SetExecutor(id, executor))
 				return Ok();
 
 			return BadRequest("The set is failed");
