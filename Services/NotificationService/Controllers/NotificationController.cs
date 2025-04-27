d1f1781280a464947009efad5e730911c35f9614
LMBSHKA <makstanc2013@yandex.ru>
2025-04-27 21:28:44 +0500

    Revert "fix: critical error"
    This reverts commit 10fd0e0f143ff913e16f93b07e214c40cfb4bd74.

--- Services/NotificationService/Controllers/NotificationController.cs
+++ Services/NotificationService/Controllers/NotificationController.cs
@@ -20,6 +20,7 @@ namespace NotificationService.Controllers
 		[HttpPost]
 		public async Task<IActionResult> CreateNotification([FromBody] Notification notification)
 		{
+			HttpClient
 			if (await _notificationRepo.CreateNotification(notification))
 				return Ok("notification created");
 
